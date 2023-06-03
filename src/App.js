d83f19797406c970d5061002690ba75c37db3443
Anbarasan <118906298+arasan420@users.noreply.github.com>
2023-06-03 16:31:13 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import { ChatEngine } from 'react-chat-engine';
+import ChatFeed  from './components/ChatFeed';
+import { LoginForm } from './components/LoginForm';
 import './App.css';
 
 function App() {
+  if (!localStorage.getItem('username')) return <LoginForm />;
   return (
     <div className="App">
      <ChatEngine
      height="100vh"
-     projectId="9b9ac047-4961-41a4-a498-c13d3e64f968"
-     usename="Gowthanm"
+     projectID="9b9ac047-4961-41a4-a498-c13d3e64f968"
+     userName="Gowtham"
      userSecret="qwerty"
+     renderChatFeed={(chatAppProps)=><ChatFeed {...chatAppProps}/>}
+     onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
      ></ChatEngine>
     </div>
   );
